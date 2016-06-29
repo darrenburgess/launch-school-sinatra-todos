9ca4ccee5e372c3a7f44c6db035f4b10d329a460
Darren Burgess <darrentburgess@gmail.com>
2016-06-29 09:53:52 -0400

    add failure validation for list creation

--- todo.rb
+++ todo.rb
@@ -28,7 +28,13 @@ end
 
 # create new list
 post "/lists" do
-  session[:lists] << {name: params[:list_name], todos: []}
-  session[:success] = "The list has been created."
-  redirect "/lists"
+  list_name = params[:list_name].strip
+  if (1..100).include? list_name.size
+    session[:lists] << {name: params[:list_name], todos: []}
+    session[:success] = "The list has been created."
+    redirect "/lists"
+  else
+    session[:error] = "List name must be between 1 and 100 characters"
+    redirect "/lists/new"
+  end
 end
