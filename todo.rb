04e1d4028a96685ab1c40dc206fb14373094061a
Darren Burgess <darrentburgess@gmail.com>
2016-06-29 10:42:50 -0400

    refactor validation conditions

--- todo.rb
+++ todo.rb
@@ -29,15 +29,15 @@ end
 # create new list
 post "/lists" do
   list_name = params[:list_name].strip
-  if @lists.any? { |list| list[:name] == list_name }
+  if !(1..100).cover? list_name.size
+    session[:error] = "List name must be between 1 and 100 characters" #ti
+    redirect "/lists/new"
+  elsif @lists.any? { |list| list[:name] == list_name }
     session[:error] = "List names must be unique"
     redirect "/lists/new"
-  elsif (1..100).cover? list_name.size
-    session[:lists] << {name: list_name, todos: []}
-    session[:success] = "The list has been created."
-    redirect "/lists"
   else
-    session[:error] = "List name must be between 1 and 100 characters"
-    redirect "/lists/new"
+    session[:lists] << {name: list_name, todos: []}
+    session[:success] = "The list has been created"
+    redirect "/lists"
   end
 end
