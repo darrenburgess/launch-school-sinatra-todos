60d6b589c2abcc6be63bacb2237a99bb25485ff2
Darren Burgess <darrentburgess@gmail.com>
2016-06-28 20:37:11 -0400

    add session; add new list route

--- todo.rb
+++ todo.rb
@@ -2,11 +2,26 @@ require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
 
+configure do
+  enable :sessions
+  set :session_secret, 'secret'
+end
+
+before do
+  session[:lists] ||= []
+end
+
 get "/" do
-  @lists = [
-    {name: "Lunch Groceries"},
-    {name: "Dinner Groceries"}
-  ]
+  redirect "/lists"
+end
+
+get "/lists" do
+  @lists = session[:lists]
   erb :lists
+end
+
+get "/lists/new" do
+  session[:lists] << { name: "New List", todos: []}
+  redirect "/lists"
 end
 
