bdee253f3eaf571a16d85aae7f4142b8ab8fc628
Darren Burgess <darrentburgess@gmail.com>
2016-07-05 08:08:50 -0400

    add delete confirmation

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,1 +1,14 @@
-console.log("testing");
+// confirmation dialog
+$(function() {
+
+  $("form.delete").submit(function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+  
+    var ok = confirm("Ar you sure? This cannot be undone!");
+    if (ok) {
+      this.submit();
+    }
+  });
+
+});
