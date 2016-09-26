0cf05ca3bec3f7d44d70ecb89f2235a1a1f7432d
Darren Burgess <darrentburgess@gmail.com>
2016-09-26 09:28:04 -0400

    change order of methods in class

--- sequel_persistence.rb
+++ sequel_persistence.rb
@@ -34,6 +34,26 @@ class SequelPersistence
     @db[:lists].where(lists__id: id).update(name: new_name)
   end
 
+  def delete_list(id)
+    @db[:todos].where(list_id: id).delete
+    @db[:lists].where(id: id).delete
+  end
+
+  def find_todos(list_id)
+    @db[:todos].where(list_id: list_id)
+  end
+
+  def create_todo(list_id, todo)
+    @db[:todos]
+    sql = "INSERT INTO todos (list_id, name) VALUES ($1, $2)"
+    query(sql, list_id, todo)
+  end
+
+  def delete_todo(list_id, todo_id)
+    sql = "DELETE FROM todos WHERE id = $1 AND list_id = $2"
+    query(sql, todo_id, list_id)
+  end
+
   def change_todo_status(list_id, todo_id)
     find_sql = "SELECT completed FROM todos WHERE id = $1 AND list_id = $2"
     completed = query(find_sql, todo_id, list_id).values.first.first == "f" 
@@ -44,25 +64,6 @@ class SequelPersistence
   def complete_all_todos(list_id)
     sql = "UPDATE todos SET completed = true WHERE list_id = $1"
     query(sql, list_id)
-  end
-
-  def delete_list(id)
-    @db[:todos].where(list_id: id).delete
-    @db[:lists].where(id: id).delete
-  end
-
-  def create_todo(list_id, todo)
-    sql = "INSERT INTO todos (list_id, name) VALUES ($1, $2)"
-    query(sql, list_id, todo)
-  end
-
-  def delete_todo(list_id, todo_id)
-    sql = "DELETE FROM todos WHERE id = $1 AND list_id = $2"
-    query(sql, todo_id, list_id)
-  end
-
-  def find_todos(list_id)
-    @db[:todos].where(list_id: list_id)
   end
 
   private
